7a49e6103f1ec002b92ac3d2232e50f150271748
AmitZur <amit@xplg.com>
2024-02-22 15:58:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/firebase/RecyclerView/DummyData.java
@@ -0,0 +1,13 @@
+package com.example.firebase.RecyclerView;
+
+
+import com.example.firebase.R;
+
+public class DummyData {
+    public static String[] nameArray ={"Room","Game Of Thrones","Queen Gambit"};
+
+   public  static String[] priceArray={"27.99","30.99","33.99"};
+
+    public static Integer[] drawableArray = {R.drawable.room,R.drawable.game_of_thrones
+            ,R.drawable.queens_gambit};
+}
